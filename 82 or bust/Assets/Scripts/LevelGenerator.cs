7bbdd335359b9d40e5d89a0a4b1b4e0ab3a8b928
kaiway-tang <kaiway@g.ucla.edu>
2024-10-06 20:02:47 -0700

    Revert "implement player spawning"
    This reverts commit f6a7d2679734c1394e0900aead18bbca0b697c56.

--- 82 or bust/Assets/Scripts/LevelGenerator.cs
+++ 82 or bust/Assets/Scripts/LevelGenerator.cs
@@ -46,8 +46,6 @@ public class LevelGenerator : MonoBehaviour
         startChunks = new List<GameObject>();
         endChunks = new List<GameObject>();
         LoadChunks();
-        breakRoomObj = Instantiate(breakRoomPrefab, new Vector3(levelAnchorx, levelAnchory), Quaternion.identity);
-        Player.self.trfm.position = new Vector3(levelAnchorx - 10f, levelAnchory + 3f);
         GenerateLevel(3);
     }
 
